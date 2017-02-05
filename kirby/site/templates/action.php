3ec9578cbec94d154a7c570d3f4295c3c455a891
andosteinmetz <ando.steinmetz@gmail.com>
2017-02-05 15:39:57 -0500

    changed email template name to reflect that it is global

--- kirby/site/templates/action.php
+++ kirby/site/templates/action.php
@@ -7,26 +7,6 @@
 				snippet('footer');
 			}
 			else{
-				switch( $page->action_type()->value() ){
-					case('electoral'):
-						// blue template
-						break;
-					case('mindfulness'):
-						// purple template
-						break;
-					case('stops_and_checks'):
-						//pink template
-						break;
-					case('two_hours'):
-						// grey template
-						break;
-					case('anti_trump'):
-						// yello/orange template
-						break;
-					case('policy'):
-						// green
-						break;
-				}
-				snippet('action-email-blue');
+				snippet('action-email');
 			} 
 		?>
