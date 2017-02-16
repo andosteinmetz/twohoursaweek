29e53ec5866330af3d5c34e2b07194866c3e7e83
andrewdeutsch <andrew.deutsch@gmail.com>
2017-02-16 09:33:50 -0500

    Revised copy

--- kirby/site/snippets/educate-act-intro.php
+++ kirby/site/snippets/educate-act-intro.php
@@ -6,11 +6,9 @@
 			205 Hudson St / Hunter MFA / 2nd Floor / New York, NY 10013
 		</p>
 		
-		<p>Co-hosted by Hunter Artist Action Group [HAAG] of Hunter MFA and 2 Hours A Week, EDUCATE & ACT is a community organized skill & information sharing program.</p>
+		<p>EDUCATE & ACT is a series of events aimed at empowering us to be better informed citizens and activists, with stronger voices and greater stamina.</p> 
 
-		<p>We are hosting a series of events aimed at empowering all of us to be better informed citizens and activists, with stronger voices and longterm stamina.</p>
-		
-		<p>Events will include teach-ins, panel discussions, self-care and movement workshops, performances, readings, screenings, letter writing and grassroots organizing. This is all made possible by the generosity of our peers and colleagues, who are donating their time, knowledge, and skills to help us together build a strong and aware community of resistance.</p>
+		<p>This program is cohosted by Hunter Artist Action Group (HAAG) of Hunter MFA and 2 Hours a Week. Events will include teach-ins, workshops, panels, performances, readings, discussions, seminars, screenings, letter writing, sign making, and grassroots organizing. They are made possible by the generosity of our peers and colleagues, who are donating their time, knowledge, and skills to help us together build a strong and aware community of resistance.</p>
 		
 		<p>If you would like to program an event or get more involved, please <a href="mailto:twohoursaweek@gmail.com">contact us</a>.</p>
 	</div>
