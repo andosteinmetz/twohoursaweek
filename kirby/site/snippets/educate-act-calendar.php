9da6866de0a72b2724f19252112a92eb2b9ff8d3
andosteinmetz <ando.steinmetz@gmail.com>
2017-03-04 18:04:01 -0500

    Past events ordered reverse chronological, cleaned code.

--- kirby/site/snippets/educate-act-calendar.php
+++ kirby/site/snippets/educate-act-calendar.php
@@ -2,15 +2,24 @@
 <h2>Upcoming Events</h2>
 <p>Please RSVP</p>
 <?php
-	$upcoming_events = $pages->find('educate-act-events')->children()->visible()->filter(function($page){return $page->date(null, 'datetime') >= date_timestamp_get(date_create());})->sortBy('datetime', 'asc'); 
+	$upcoming_events = $pages->find('educate-act-events')->children()->visible()->filter(function($page){
+		return $page->date(null, 'datetime') >= date_timestamp_get(date_create());
+	})
+	->sortBy('datetime', 'asc'); 
+	
 	foreach ($upcoming_events as $page){
 		snippet('event', array('page'=>$page));
 	}
 ?>
 
 <?php
-	$past_events = $pages->find('educate-act-events')->children()->filter(function($page){return $page->date(null, 'datetime') <= date_timestamp_get(date_create());})->sortBy('datetime', 'asc');
-	if(count($past_events) > 0){ echo '<h2>Past</h2>'; } 
+	$past_events = $pages->find('educate-act-events')->children()->filter(function($page){
+		return $page->date(null, 'datetime') <= date_timestamp_get(date_create());
+	})
+	->sortBy('datetime', 'desc');
+	
+	if(count($past_events) > 0){ echo '<h2>Past Events</h2>'; } 
+	
 	foreach ($past_events as $page){
 		snippet('event', array('page'=>$page));
 	}
