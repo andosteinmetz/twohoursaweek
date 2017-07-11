7fdca9a2a15ac7a1430a07bb9529f630df8fc106
Andrew Steinmetz <ando.steinmetz@gmail.com>
2017-07-11 12:50:18 -0400

    adds fftf battleforthenet widget

--- kirby/site/snippets/footer.php
+++ kirby/site/snippets/footer.php
@@ -10,6 +10,7 @@
   </footer>
   <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/core.js"></script>
   <script type="text/javascript" src="https://cdnjs.com/libraries/slick-carousel"></script>-->
+  <script src="https://widget.battleforthenet.com/widget.js" async></script>
   <script type="text/javascript" src="/assets/js/jquery-3.1.1.min.js"></script>
   <script type="text/javascript" src="/assets/js/slick-1.6.0/slick/slick.min.js"></script>
   <script type="text/javascript" src="/assets/js/main.js"></script>
